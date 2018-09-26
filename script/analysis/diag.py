ce24f62fb94408a1b1b9fb8ee7fbbdcb520bbff3
Jonah Maxwell Miller <jonahm@lanl.gov>
2018-09-26 17:24:08 -0600

    singleton diag added and summary diag renamed

--- script/analysis/diag.py
+++ script/analysis/diag.py
@@ -1,161 +1,125 @@
-import matplotlib
-import matplotlib.pyplot as plt
-import plot as bplt
-import sys, os
-import hdf5_to_dict as io
+#!/usr/bin/env python
+
+################################################################################
+#                                                                              # 
+#  SMALL LIBRARY FOR PLOTTING SINGLE DIAG/IN-SITU QUANTITIES                   # 
+#                                                                              # 
+################################################################################
+
+from __future__ import print_function
+import sys; sys.dont_write_bytecode = True
 import numpy as np
-font = {'size' : 14}
-matplotlib.rc('font', **font)
+import matplotlib as mpl
+mpl.use('agg')
+from  matplotlib import pyplot as plt
+mpl.rcParams.update({'font.size':18})
 
-if len(sys.argv) != 3:
-  print('ERROR format is')
-  print('  diag.py [dumpfolder] [fluid/rad/perf]')
-  sys.exit()
+def plot_q_of_t(qname   = 'mdot_eh',
+                diag    = None,
+                path    = None,
+                op      = None,
+                save    = False,
+                display = False,
+                ymin    = None,
+                ymax    = None,
+                log     = False):
+    """Plot quantity q, given in the diag dict object by qname,
+    as a function of time. Options are:
 
-folder = sys.argv[1]
-if not os.path.exists(folder):
-  print('ERROR File ' + folder + ' does not exist!')
-  sys.exit()
+    qname -- String, Name of quantity to plot out of diag. Default is 'modt_eh'
 
-if sys.argv[2] in ['fluid', 'rad', 'perf']:
-  mode = sys.argv[2]
-else:
-  print('ERROR supported modes are')
-  print('  fluid rad perf')
-  sys.exit()
+    diag  -- String, diag object attained by using hdf5_to_dict.load_diag.
+             Default is None, but must be set unless path is not None.
+          
+    path  -- String, path to directory containing diag file. Default is None,
+             but must be set unless diag is not None.
+          
+    op    -- unary function, operation to apply to q before plotting.
+             default is the identity.
+          
+    save  -- Save the plot to a file? Plot is saved into
+            current working directory. Default is False.
 
-diag = io.load_diag(folder)
+    ymin -- Minimum y value. Defaults to letting the code choose.
 
-if mode == 'perf':
-  fig = plt.figure(figsize=(14,12))
-  
-  vals = ['TIMER_UPDATE', 'TIMER_FLUXCALC', 'TIMER_FIXUP', 'TIMER_BOUND', 
-    'TIMER_DIAG', 'TIMER_OUT', 'TIMER_MAKE', 'TIMER_PUSH', 
-    'TIMER_INTERACT', 'TIMER_ALL']
-  if diag['hdr']['ELECTRONS']:
-    vals.append('TIMER_ELECTRON')
-  for n, val in enumerate(vals):
-    ax = fig.add_subplot(4, 3, n+1)
-    ax.plot(diag['t'], diag[val])
-    ax.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
-    if n < 9:
-      ax.set_xticks([])
+    ymax -- Maximum y value. Defaults to letting the code choose.
+
+    display -- Whether or not to show the plot. Default is False.    
+
+    """
+    if diag is None:
+        if path is None:
+            raise ValueError("Either path or diag must be non-none.")
+        import hdf5_to_dict as io
+        diag = io.load_diag(path)
+    t = diag['t']
+    q = diag[qname]
+    if op is not None:
+        q = op(q)
+    if log:
+        plt.semilogy(t,q)
     else:
-      ax.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
-    ax.set_title(val)
+        plt.plot(t,q)
+    plt.xlabel('t (code units)')
+    plt.ylabel(qname + ' (code units)')
+    if ymin is not None or ymax is not None:
+        if ymin is None:
+            ymin = np.min(q)
+        if ymax is None:
+            ymin = np.max(q)
+        plt.ylim(ymin,ymax)
+    if save:
+        for postfix in ['pdf', 'png']:
+            plt.savefig(qname + '.' + postfix,
+                        bbox_inches='tight')
+    if display:
+        plt.show()
+    return
 
-elif mode == 'fluid':
-  fig = plt.figure(figsize=(10,10))
+if __name__ =="__main__":
+    import argparse
+    parser = argparse.ArgumentParser(
+        description='Plot an in-situ quantity in bhlight.')
+    parser.add_argument('dumpdir',
+                        type=str,
+                        help='Path to directory containing dumps and diag file.')
+    parser.add_argument('-q','--qname',
+                        dest='qname',
+                        type=str,
+                        help='Name of quantity to plot.',
+                        default='mdot_eh')
+    parser.add_argument('-i','--invert',
+                        action='store_true',
+                        dest='invert',
+                        help='Invert the y-axis of the plot.')
+    parser.add_argument('-l','--log',
+                        action='store_true',
+                        dest='log',
+                        help='Make y axis on a log scale')
+    parser.add_argument('--ymin',
+                        type=float,
+                        help='Min of plot.',
+                        default=None)
+    parser.add_argument('--ymax',
+                        type=float,
+                        help='Max of plot.',
+                        default=None)
+    args = parser.parse_args()
 
-  ax = fig.add_subplot(4,2,1)
-  ax.plot(diag['t'], -np.array(diag['mdot']))
-  #ax.set_yscale('log')
-  ax.set_ylabel('mdot')
-
-  ax = fig.add_subplot(4,2,2)
-  ax.plot(diag['t'], np.array(diag['edot']))
-  ax.set_yscale('log')
-  ax.set_ylabel('edot')
-
-  ax = fig.add_subplot(4,2,3)
-  ax.plot(diag['t'], -np.array(diag['ldot']))
-  ax.set_yscale('log')
-  ax.set_ylabel('ldot')
-
-  ax = fig.add_subplot(4,2,4)
-  ax.plot(diag['t'], np.array(diag['mass']))
-  ax.set_yscale('log')
-  ax.set_ylabel('mass')
-
-  ax = fig.add_subplot(4,2,5)
-  ax.plot(diag['t'], np.array(diag['egas']))
-  ax.set_yscale('log')
-  ax.set_ylabel('egas')
-
-  ax = fig.add_subplot(4,2,6)
-  diag['phi'] = diag['Phi']/np.sqrt(-diag['mdot'])
-  ax.plot(diag['t'], np.array(diag['phi']))
-  #ax.set_yscale('log')
-  ax.set_ylabel('phi')
-
-  ax = fig.add_subplot(4,2,7)
-  #ax.plot(diag['t'], np.array(diag['jet_EM_flux']))
-  ax.plot(diag['t'], np.array(np.fabs(diag['jet_EM_flux']/diag['mdot'])))
-  ax.set_yscale('log')
-  ax.set_ylabel('jet')
-
-  ax = fig.add_subplot(4,2,8)
-  ax.plot(diag['t'], np.array(diag['divbmax']))
-  ax.set_yscale('log')
-  ax.set_ylabel('divbmax')
-
-  for n in range(8):
-    ax = fig.add_subplot(4,2,n+1)
-    if n % 2 != 0:
-      ax.yaxis.tick_right()
-      ax.yaxis.set_label_position('right')
-    if n < 6:
-      ax.set_xticks([])
+    if args.invert:
+        op = lambda x: -x
     else:
-      ax.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
+        op = None
 
-elif mode == 'rad':
-
-  fig = plt.figure(figsize=(10,10))
-
-  ax = fig.add_subplot(4,2,1)
-  ax.plot(diag['t'], -np.array(diag['Mdot'])/diag['hdr']['MdotEdd'])
-  ax.set_yscale('log')
-  ax.set_ylabel('mdot')
-
-  ax = fig.add_subplot(4,2,2)
-  ax.plot(diag['t'], np.array(diag['Lum'])/diag['hdr']['LEdd'])
-  ax.set_yscale('log')
-  ax.set_ylabel('L/LEdd')
-
-  ax = fig.add_subplot(4,2,3)
-  ax.plot(diag['t'], diag['tune_emiss'])
-  ax.set_yscale('log')
-  ax.set_ylabel('tune_emiss')
-
-  ax = fig.add_subplot(4,2,4)
-  ax.plot(diag['t'], diag['step_made_all'])
-  ax.set_yscale('log')
-  ax.set_ylabel('made')
-
-  ax = fig.add_subplot(4,2,5)
-  ax.plot(diag['t'], diag['tune_scatt'])
-  ax.set_yscale('log')
-  ax.set_ylabel('tune_scatt')
-
-  ax = fig.add_subplot(4,2,6)
-  print(diag['step_scatt_all'].max())
-  ax.plot(diag['t'], diag['step_scatt_all'])
-  ax.set_yscale('log')
-  ax.set_ylabel('scatt')
-
-  ax = fig.add_subplot(4,2,7)
-  ax.plot(diag['t'], diag['egas'])
-  ax.plot(diag['t'], -diag['erad'])
-  ax.set_yscale('log')
-  ax.set_ylabel('egas erad')
-
-  ax = fig.add_subplot(4,2,8)
-  ax.plot(diag['t'], diag['step_tot_all'])
-  ax.set_yscale('log')
-  ax.yaxis.tick_right()
-  ax.yaxis.set_label_position('right')
-  ax.set_ylabel('tot')
-    
-  for n in range(8):
-    ax = fig.add_subplot(4,2,n+1)
-    if n % 2 != 0:
-      ax.yaxis.tick_right()
-      ax.yaxis.set_label_position('right')
-    if n < 6:
-      ax.set_xticks([])
-    else:
-      ax.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
-
-plt.show()
-
+    print("Bhlight: Plotting {} ".format(args.qname),
+          "in simulation directory {}.".format(args.dumpdir))
+    plot_q_of_t(
+        qname = args.qname,
+        path = args.dumpdir,
+        op = op,
+        save = True,
+        ymin = args.ymin,
+        ymax = args.ymax,
+        log = args.log
+    )
+    print("Done!")    
